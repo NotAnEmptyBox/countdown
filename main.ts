fba69cb9afbfd57fd4833b291d93a2ed304297cb
NotAnEmptyBox <93144513+NotAnEmptyBox@users.noreply.github.com>
2021-10-25 11:43:40 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+for (let index = 0; index <= 2; index++) {
+    basic.showNumber(3 - index)
+}
+basic.clearScreen()
+music.playTone(392, music.beat(BeatFraction.Whole))
+basic.showString("GO!")
+basic.forever(function () {
+	
+})
